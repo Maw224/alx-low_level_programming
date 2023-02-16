852986e62ceb9e4e2312cb324a570c308eac421f
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-02-16 10:33:33 -0800

    Quote to standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 /**
  * main - main block
  * Return: 1
  */
 int main(void)
 {
-	char string[] = "and that piece of art is useful\" - Dora Korbar, 2015-10-19";
+	int str_len = strlen("and that piece of art is useful\" - Dora Korbar, 2015-10-19");
 
-	fprintf(stderr, "%s\n", string);
+	write(1, "and that piece of art is useful\" - Dora Korbar, 2015-10-19\n", str_len);
 	return (1);
 }
