1c933aa6c7aafe74b7348597840ddaca748e527a
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-02-16 15:13:17 -0800

    New quote to standard output

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
-#include <string.h>
-#include <unistd.h>
 /**
  * main - main block
  * Return: 1
  */
 int main(void)
 {
-	int s;
-
-	s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-
-	write(2,
-		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-		s
-		);
+	fprintf(stdout,
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr,
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
