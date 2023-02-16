832b3f625f09dc373489d4a328b69963ffe676e6
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-02-16 10:40:43 -0800

    Quote and standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,13 @@
  */
 int main(void)
 {
-	int str_len = strlen("and that piece of art is useful\" - Dora Korbar, 2015-10-19");
+	int str_len = strlen(
+			"and that piece of art is useful\", - Dora Korbar, 2015-10-19\n"
+		);
 
-	write(1, "and that piece of art is useful\" - Dora Korbar, 2015-10-19\n", str_len);
+	write(1,
+		"and that piece of art is useful\", - Dora Korbar, 2015-10-19\n",
+		str_len
+		);
 	return (1);
 }
