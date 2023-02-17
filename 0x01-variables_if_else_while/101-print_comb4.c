884f311985489cd7dc4632dcf1b44ba6761ea812
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-02-17 08:41:22 -0800

    comb 3 numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x, first, second, third;
+
+	x = 0;
+	while (x < 1000)
+	{
+		first = x / 100;
+		second = (x / 10) % 10;
+		third = x % 10;
+		if (first < second && second < third)
+		{
+			putchar('0' +  first);
+			putchar('0' + second);
+			putchar('0' + third);
+			if (x < 789)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
