9aff6aec8d86c195a27ae3da6d11610c2547e29c
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-02-17 15:28:38 -0800

    two numbers combination

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x, y = 0;
+
+	while (x < 100)
+	{
+		while (y < 100)
+		{
+			if (x < y)
+			{
+				putchar('0' + x);
+				putchar(32);
+				putchar('0' + y);
+			}
+			if (x < 89)
+			{
+				putchar(44);
+				putchar(32);
+			}
+			y++;
+		}
+		x++;
+	}
+	puthchar('\n');
+	return (0);
+}
