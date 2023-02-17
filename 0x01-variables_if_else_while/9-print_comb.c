8aa0e7775e7518353f9516129ada4b1a159fd3aa
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-02-17 07:32:07 -0800

    Concat integers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,12 +13,9 @@ int main(void)
 
 	for (x = 0; x < 10; x++)
 	{
-		for (y = 0; y < 10; y++)
-		{
-			putchar('0' + x);
-			putchar(44);
-			putchar(32);
-		}
+		putchar('0' + x);
+		putchar(44);
+		putchar(32);
 	}
 	putchar('\n');
 	return (0);
