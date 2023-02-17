7b83d5a320101f4d4012186228a994a32adba6f7
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-02-17 06:14:00 -0800

    Alphabet exept e q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Return: 0
+ */
+
+int main(void)
+{
+	char x = 'a';
+
+	for (x; x <= 'z'; x++)
+	{
+		if (x != 'e' || x != 'q')
+			putchar(x);
+	}
+	printf("\n");
+	return (0);
+}
