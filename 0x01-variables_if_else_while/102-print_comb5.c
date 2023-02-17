02236df14fdce724f9dd16ce7e6ca35f0fd32580
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-02-17 15:37:55 -0800

    Two numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,17 +7,24 @@
 
 int main(void)
 {
+	int first_x, second_x, first_y, second_y;
 	int x, y = 0;
 
 	while (x < 100)
 	{
+		first_x = x / 10;
+		second_x = x % 10;
 		while (y < 100)
 		{
-			if (x < y)
+			first_y = y / 10;
+			second_y = y % 10;
+			if (second_x < second_y)
 			{
-				putchar('0' + x);
+				putchar('0' + first_x);
+				putchar('0' + second_x);
 				putchar(32);
-				putchar('0' + y);
+				putchar('0' + first_y);
+				putchar('0' + second_y);
 			}
 			if (x < 89)
 			{
