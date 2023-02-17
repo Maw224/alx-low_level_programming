e3ecc4a1b54e543a1b233f17a77890da9e379a20
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-02-17 05:53:47 -0800

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <sttdio.h>
+
+/**
+ * main - main block
+ * Return: 0
+ */
+
+int main(void)
+{
+	char x = 'a';
+
+	while (x < 'z')
+	{
+		putchar(x);
+		x++;
+	}
+	printf("\n");
+	return (0);
+}
