7856199e6b77e1af744ccea3648a26ec6fc3f00b
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-02-17 07:47:15 -0800

    two digit combination without repetion

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			if (x != y && x < y)
+			{
+				putchar('0' + x);
+				putchar('0' + y);
+				if (x < 9)
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
