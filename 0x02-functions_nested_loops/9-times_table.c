ccdb771a134bfb5e352a5dc7c3300ad3aa978046
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-02-21 08:23:09 -0800

    Times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,7 +15,11 @@ void times_table(void)
 		for (y = 0; y < 10; y++)
 		{
 			if ((x * y) < 10)
+			{
+				if (y != 0)
+					_putchar(' ');
 				_putchar('0' + x * y);
+			}
 			else
 			{
 				_putchar('0' + x * y / 10);
@@ -24,13 +28,7 @@ void times_table(void)
 			if (y < 9)
 			{
 				_putchar(',');
-				if ((x * y) < 10)
-				{
-					_putchar(' ');
-					_putchar(' ');
-				}
-				else
-					_putchar(' ');
+				_putchar(' ');
 			}
 		}
 		_putchar('\n');
