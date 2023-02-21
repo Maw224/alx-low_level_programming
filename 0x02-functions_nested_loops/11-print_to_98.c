a6d267049ec45d44b37692ccf4f315b8375a295d
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-02-21 08:38:20 -0800

    Print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main."
+#include "stdio.h"
+
+/**
+ * print_to_98 - main block
+ * @n: number
+ * Return: 0
+ */
+
+int print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+	}
+	else
+	{
+		while (n >= 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+	}
+}
