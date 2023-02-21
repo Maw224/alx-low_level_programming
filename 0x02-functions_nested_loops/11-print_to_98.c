2f48126df9ac5b20ae7c2e33ff8688bdc92b74d6
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-02-21 08:43:12 -0800

    Print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,7 +13,10 @@ int print_to_98(int n)
 	{
 		while (n <= 98)
 		{
-			printf("%d, ", n);
+			if (n != 98)
+				printf("%d, ", n);
+			else
+				printf("%d", n);
 			n++;
 		}
 	}
@@ -21,7 +24,10 @@ int print_to_98(int n)
 	{
 		while (n >= 98)
 		{
-			printf("%d, ", n);
+			if (n != 98)
+				printf("%d, ", n);
+			else
+				printf("%d", n);
 			n--;
 		}
 	}
