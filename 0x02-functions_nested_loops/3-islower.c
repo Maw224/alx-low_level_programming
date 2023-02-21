83c973ef89052bf51034b41f22a716aa1d79c388
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-02-21 07:03:55 -0800

    Is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - main block
+ * Return: 1 is lower, 0 otherwise
+ */
+
+int _islower (int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
