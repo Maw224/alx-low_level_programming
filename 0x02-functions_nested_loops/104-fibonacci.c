07cfd555a86dca4b430383686881ec7104133b46
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-02-21 14:17:54 -0800

    Fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,26 +9,38 @@
 
 int main(void)
 {
-	unsigned long int current = 1, previos = 0, save_current, fr1, fr2;
+	unsigned long int current = 1, previos = 0, save_current, next, fr1, fr2;
 	int i = 0;
 
 	while (i < 98)
 	{
 		if ((current + previos) > MAX || fr1 > 0 || fr2 > 0)
 		{
-			fr1 = (current + previos) / MAX;
-			fr2 = (current + previos) % MAX;
+			if(fr1 > 0 || fr2 > 0)
+			{
+				save_current = fr1;
+				fr1 = ((save_current * 10) + fr2) / MAX;
+				fr2 = ((save_current * 10) + fr2) % MAX;
+			}
+			else
+			{
+				fr1 = (current + previos) / MAX;
+				fr2 = (current + previos) % MAX;
+			}
+			
 			printf("%lu%lu", fr1, fr2);
 		}
 		else
+		{
+			next = current + previos;
+			save_current = current;
+			current += previos;
+			previos = save_current;
 			printf("%lu", current + previos);
+		}
 
 		if (i < 97)
 			printf(", ");
-
-		save_current = current;
-		current += previos;
-		previos = save_current;
 		i++;
 	}
 	printf("\n");
