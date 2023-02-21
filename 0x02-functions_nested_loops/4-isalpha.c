b96323658010f76ad5439d2415b14bac970f9c13
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-02-21 07:13:08 -0800

    Is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - main block
+ * @c: Ascii for char
+ * return: 1 success, 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+		return (1);
+	else
+		return (0);
+}
