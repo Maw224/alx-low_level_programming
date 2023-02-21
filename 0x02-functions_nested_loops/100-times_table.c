12aab145ea7b09b2d63e277ba41ece7b1ab6f043
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-02-21 08:58:54 -0800

    Times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * print_times_table - main block
+ * @n: number >= 0 and <= 15
+ * Return: void
+ */
+
+void print_times_table(int n)
+{
+	if (n >= 0 && n <= 15)
+	{
+		int x, y;
+
+		for (x = 0; x <= n; x++)
+		{
+			if (x * y >= 100)
+				printf(" %d", x * y);
+			else if (x * y >= 10)
+				printf("  %d", x * y);
+			else
+				printf("   %d", x * y);
+			if (y != n)
+				printf(",");
+		}
+	}
+}
