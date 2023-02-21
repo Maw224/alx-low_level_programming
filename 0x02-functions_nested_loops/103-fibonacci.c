83a6b29ef49436440487cdf88e52694a1678d594
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-02-21 09:54:33 -0800

    Fibbonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include "stdio.h"
+
+/**
+ * main - main block
+ * Return: 0
+ */
+
+int main(void)
+{
+	long int current = 1, previos = 0;
+	long int next, save_current, result;
+
+	result = 0;
+	while (next <= 4000000)
+	{
+		next = current + previos;
+		save_current = current;
+		current += previos;
+		previos = save_current;
+		
+		if (next % 2 == 0)
+			result += next;
+	}
+	printf("%d\n", result);
+	return (0);
+}
