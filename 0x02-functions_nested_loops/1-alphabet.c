ddbcce9a96705eabe49bab824e7e1fc41ac8d766
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-02-21 06:45:49 -0800

    Print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - main block
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char x = "a";
+
+	while (x <= "z")
+	{
+		_putchar(x);
+		x++;
+	}
+	_putchar('\n');
+}
