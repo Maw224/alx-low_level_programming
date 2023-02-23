3fa038566ddebeb91dbfa58ab6091c0f5ecf2fa0
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-02-23 09:55:25 -0800

    Square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_square - main block
+ * @n: number
+ */
+
+void print_square(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
