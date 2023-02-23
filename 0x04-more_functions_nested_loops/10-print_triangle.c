2d58973ec72235d8c7b0f18604d04ef80b01156b
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-02-23 10:14:13 -0800

    Triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_triangle - main block
+ * @n: size
+ */
+
+void print_triangle(int n)
+{
+	int i, j, k;
+
+	if (n <= 0)
+		_putchar(']n');
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = n - i; j > 1; j--)
+		{
+			_putchar(' ');
+			for (k = 0; k < i; k++)
+			{
+				_putchar('#');
+			}
+		}
+		_putchar('\n');
+	}
+}
+
