61a42b7d7975694c623da4409512477977161da3
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-02-23 09:18:59 -0800

    Numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,11 +6,16 @@
 
 void more_numbers(void)
 {
-	int i = 0, j = 0;
+	int i, j;
 
-	while (i < 10)
+	for (i = 0; i < 10; i++)
 	{
-		_putchar('0' + i);
-		i++;
+		for (j = 0; j < 15; j++)
+		{
+			if (j > 9)
+				_putchar('0' + (j \ 10));
+			_putchar('0' + (j % 10));
+		}
+		_putchar('\n');
 	}
 }
