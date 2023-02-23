07b9a48e7007f7079eb0d6ac59933906c3fd593e
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-02-23 08:24:30 -0800

    More umbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - main block
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j < 10)
+				_putchar('0' + j);
+			else
+			{
+				int x = j / 10;
+				int y = j % 10;
+
+				_putchar('0' + x);
+				_puthcar('0' + y);
+			}
+		}
+		_putchar('\n');
+	}
+}
