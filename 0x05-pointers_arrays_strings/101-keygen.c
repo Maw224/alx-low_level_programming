dd62e8044b2098799166382fd5d44cb32a5d1d3b
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-02-27 13:05:03 -0800

    Random password generator

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - main block
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, random;
+
+	srand((unsigned int)(time(NULL)));
+
+	char numbers[] = "0123456789";
+	char letters[] = "qwertyuiopasdfhjklzxcvbnm";
+	char LETTERS[] = "QWERTYUIOPASDFGHJKLZXCVBNM";
+	char symbols = "!@#$^&*?";
+	char pasword[8];
+
+	random = rand() % 4;
+
+	for (i = 0; i < 8; i++)
+	{
+		if (random == 1)
+		{
+			password[i] = numbers[rand() % 10];
+			random = rand % 4;
+			printf("%c", password[i]);
+		}
+		if (random == 2)
+		{
+			password[i] = letters[rand() % 26];
+			random = rand % 4;
+			printf("%c", password[i]);
+		}
+		if (random == 3)
+			{
+			password[i] = LETTERS[rand() % 26];
+			random = rand % 4;
+			printf("%c", password[i]);
+		}
+		else
+		{
+			password[i] = symbols[rand() % 8];
+			random = rand % 4;
+			printf("%c", password[i]);
+		}
+	}
+	return (0);
+}
