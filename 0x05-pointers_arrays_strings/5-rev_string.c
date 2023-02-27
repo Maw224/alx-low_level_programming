09ac83ec884d09408cf8b48f5b9a39e9c74bceb3
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-02-27 07:43:50 -0800

    rev string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <strig.h>
+
+/**
+ * rev_string - main block
+ * @s: string
+ */
+
+void rev_string(char *s)
+{
+	unsigned int i, j;
+
+	j = 0;
+
+	for (i = strlen(s); i > 0, i--)
+	{
+		s[j] = s[i];
+		j++
+	}
+}
