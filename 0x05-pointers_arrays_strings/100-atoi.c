d96d8b480d4e3ac0fe9539048a0efc248d2aabdd
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-02-27 09:26:41 -0800

    Atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _atoi - main block
+ * @c: string
+ * Return: number from the string
+ */
+
+int _atoi(char *c)
+{
+	int i = atoi(c);
+
+	return (i);
+}
