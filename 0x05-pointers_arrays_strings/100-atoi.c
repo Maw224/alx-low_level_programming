ba123d0a47f4a9048e42d2785bd1bce471a72dba
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-02-27 10:49:58 -0800

    Atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,20 +11,16 @@
 
 int _atoi(char *c)
 {
-	char *ch2 = "";
-	int j, num_count = 0, sign_count = 0, value;
-	int len, i;
+	/*char *ch2 = "";*/
+	/*int j, num_count = 0, sign_count = 0, value;*/
+	/*int len, i;*/
+	int value;
 
 	if ((c[0] == '-' || c[0] == '+' || c[0] == ' ')
 			&& !(c[1] >= '0' && c[1] <= '9'))
 	{
-		len = strlen(c);
-		printf("Length:%d\n", len);
-		ch2[0] = 'm';
-		i = 0;
-		j = 1;
+		printf("not normal, lenght: %u", strlen(c));
 		value = 2;
-		printf("i: %d, j: %d, num: %d, sign: %d, ch2: %s",i, j, num_count, sign_count, ch2);
 	}
 	else
 	{
