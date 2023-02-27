8e21093795a492336ce4f6a137bbb06049fa1429
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-02-27 12:31:48 -0800

    Atoi with Stoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -39,3 +39,31 @@ int _atoi(char *c)
 	}
 	return (value);
 }
+
+/**
+ * stoi_max - secondary block
+ * @c: string
+ * @max: integer
+ * @sign: integer
+ * Return: signed integer value from string
+ */
+
+int stoi_max(char *c, int max, int sign)
+{
+	int sub_value = 0;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		if (c[i] >= '0' && c[i] <= '9')
+		{
+			max /= 10;
+			sub_value += (c[i] - '0')* max;
+			if (c[i + 1] < '0' || c[i + 1] > '9')
+				break;
+		}
+	}
+	if (sign % 2 != 0)
+		sub_value *= -1;
+
+	return (sub_value);
+}
