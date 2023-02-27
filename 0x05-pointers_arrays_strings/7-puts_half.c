5365f52e75d7576d3d71d14f9f526055c9be9d1b
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-02-27 08:53:35 -0800

    Puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,10 +11,7 @@ void puts_half(char *s)
 	int i, length, half;
 
 	length = strlen(s);
-	if (length % 2 == 0)
-		half = length / 2;
-	else
-		half = (length - 1) / 2;
+	half = length / 2;
 
 	for (i = half; i < length; i++)
 		_putchar(s[i]);
