7eff15ac295d6e498f158b25fe39c6731d06f6f5
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-02-27 13:52:13 -0800

    Random password generator

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -11,47 +11,40 @@
 int main(void)
 {
 	int i;
-	/**
-	 * char numbers[] = "0123456789";
-	 * char letters[] = "qwertyuiopasdfhjklzxcvbnm";
-	 * char LETTERS[] = "QWERTYUIOPASDFGHJKLZXCVBNM";
-	 * char symbols[] = "!@#$^&*?";
-	 */
+	char numbers[] = "0123456789";
+	char letters[] = "qwertyuiopasdfhjklzxcvbnm";
+	char LETTERS[] = "QWERTYUIOPASDFGHJKLZXCVBNM";
+	char symbols[] = "!@#$^&*?";
 	char password[15];
 
 	srand((unsigned int)(time(NULL)));
 
 	for (i = 0; i < 15; i++)
 	{
-		/**
-		 * if (random == 1)
-		
-		 * {
-		 *	password[i] = numbers[rand() % 10];
-		 *	random = rand() % 4;
-		 *	printf("%c", password[i]);
-		 * }
-		 * if (random == 2)
-		 * {
-		 *	password[i] = letters[rand() % 26];
-		 *	random = rand() % 4;
-		 *	printf("%c", password[i]);
-		 * }
-		 * if (random == 3)
-		 * 	{
-		 *	password[i] = LETTERS[rand() % 26];
-		 *	random = rand() % 4;
-		 *	printf("%c", password[i]);
-		 * }
-		 *else
-		 * {
-		 *	password[i] = symbols[rand() % 8];
-		 *	random = rand() % 4;
-		 *	printf("%c", password[i]);
-		 * }
-		 */
-		password[i] = (rand() % 94) + 32;
-		printf("%c", password[i]);
+		if (random == 1)
+		{
+			password[i] = numbers[rand() % 10];
+			random = rand() % 4;
+			printf("%c", password[i]);
+		}
+		if (random == 2)
+		{
+			password[i] = letters[rand() % 26];
+			random = rand() % 4;
+			printf("%c", password[i]);
+		}
+		if (random == 3)
+		{
+			password[i] = LETTERS[rand() % 26];
+			random = rand() % 4;
+			printf("%c", password[i]);
+		}
+		else
+		{
+			password[i] = symbols[rand() % 8];
+			random = rand() % 4;
+			printf("%c", password[i]);
+		}
 	}
 	printf("%s", password);
 	return (0);
