496c432c95617e8461d604ea5ffb8e296f4cd591
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-02-27 12:06:36 -0800

    Atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -40,8 +40,8 @@ int _atoi(char *c)
 		{
 			if (c[i] >= '0' && c[i] <= '9')
 			{
-				sub_value += (c[i] - '0')* max;
 				max /= 10;
+				sub_value += (c[i] - '0')* max;
 				if (c[i + 1] < '0' || c[i + 1] > '9')
 					break;
 			}
