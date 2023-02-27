73b303cc964fa36c59d76a308a2b5516972645d1
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-02-27 08:59:44 -0800

    Print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_array - main block
+ * @a: array
+ * @n: number
+ */
+
+void print_array(int *arr, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		_putchar(a[i]);
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,3 +7,4 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *s);
 void puts_half(char *s);
+void print_array(int *arr, int n);
