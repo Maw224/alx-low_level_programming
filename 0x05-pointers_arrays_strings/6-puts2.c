b657c915de17e903dd680a8ae20b28d99217923f
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-02-27 08:38:07 -0800

    Puts 2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts2 - main block
+ * @s: string
+ */
+
+void puts2(char *s)
+{
+	unsigned int i;
+
+	for (i = 0; i < strlen(s); i += 2)
+	{
+		_putchar(s[i]);
+	}
+}
