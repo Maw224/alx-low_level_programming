34a42d2339432ef6ba0cd7f17aca2e9ccb1b4057
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-02-27 06:26:38 -0800

    Stdout write

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _puts - main block
+ * @str: string
+ */
+
+void _puts(char *str)
+{
+	write(stdout, str);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
 void reset_to_98(int *n);
 void swap_int(int *n, int *m);
 int _strlen(char *c);
+void _puts(char *str);
