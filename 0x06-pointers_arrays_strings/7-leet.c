edaebb95bea141c09cff9e62ad75e20f83d0fb79
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-03-01 10:51:22 -0800

    Encode

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,21 +8,26 @@
 
 char *leet(char *s)
 {
-	int i, j, k;
-	char encrypt[5][3] = {{'o', 'O', '0'}, {'l', 'L', '1'}, {'e', 'E', '3'},
-		{'a', 'A', '4'}, {'t', 'T', '7'}};
+	int i;
+	/*
+	 * char encrypt[5][3] = {{'o', 'O', '0'}, {'l', 'L', '1'}, {'e', 'E', '3'},
+	 *	{'a', 'A', '4'}, {'t', 'T', '7'}};
+	 */
 
-	for (i = 0, j = 0; s[i] != '\0' && j < 5; j++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (k = 0; k < 2; k++)
-		{
-			if (s[i] == encrypt[j][k])
-			{
-				s[i] = encrypt[j][2];
-				i++;
-				j = 0;
-			}
-		}
+		if (s[i] == 'a' || s[i] == 'A')
+			s[i] = '4';
+		else if (s[i] == 't' || s[i] == 'T')
+			s[i] = '7';
+		else if (s[i] == 'l' || s[i] == 'L')
+			s[i] = '1';
+		else if (s[i] == 'o' || s[i] == 'O')
+			s[i] = '0';
+		else if (s[i] == 'e' || s[i] == 'E')
+			s[i] = '3';
+		else
+			continue;
 	}
 	return (s);
 }
