ca11eb90e6cd63f864b06992fc263084f7417111
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-03-01 10:10:23 -0800

    Cap after sep

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,15 +8,21 @@
 
 char *cap_string(char *s)
 {
-	int i;
+	int i, j;
+	char seps[] = " \t\n\v,;.!?\"(){}";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (i == 0 && (s[i] >= 97 && s[i] <= 122))
 			s[i] -= 32;
-		else if (!((s[i] >= 97 && s[i] <= 122) || (s[i] >= 65 && s[i] <= 90))
-				&& (s[i + 1] >= 97 && s[i + 1] <= 122))
-			s[i + 1] -= 32;
+		else
+		{
+			for (j = 0; seps[j] != '\0'; j++)
+			{
+				if(s[i] == seps[j] $$ (s[i + 1] >= 97 && s[i + 1] <= 122))
+					s[i + 1] -= 32;
+			}
+		}
 	}
 	return (s);
 }
