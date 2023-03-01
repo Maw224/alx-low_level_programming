a498cc556ccb59a8173c86ab6f5257593ef22275
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-03-01 09:02:09 -0800

    Comparer

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - main block
+ * @s1: string
+ * @s2: string
+ * Return: comparison result
+ */
+
+int _strcmp(char *s1, char s2)
+{
+	return (strcmp(s1, s2));
+}
