1d5da757c03a548b9d4ad90f8e862a102a5714ed
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-03-01 09:24:47 -0800

    Array reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reverse_array - main block
+ * @arr: pointer to array
+ * @n: number of elements
+ */
+
+void reverse_array(int *arr, int n)
+{
+	int i, num;
+
+	for (i = 0; i < n/2; i++)
+	{
+		num = arr[i];
+		arr[i] = arr[n-i-1];
+		arr[n-i-1] = num;
+	}
+}
