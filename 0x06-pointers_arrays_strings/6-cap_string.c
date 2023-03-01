f5b6e75f84bb3725d8515746d1dcf6be837b505d
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-03-01 09:50:02 -0800

    Capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,13 +9,13 @@
 char *cap_string(char *s)
 {
 	int i;
-	
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (i == 0 && s[i] >= 97 && s[i] <= 122)
 			s[i] -= 32;
 		else if (!(s[i] >= 97 && s[i] <= 122) && (s[i + 1] >= 97 && s[i + 1] <= 122))
-		       s[i + 1] -= 32;
+			s[i + 1] -= 32;
 	}
 	return (s);
 }
