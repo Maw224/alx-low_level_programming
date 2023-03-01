f47196768bc3ee07fe126a2bb928e2a86893eb20
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-03-01 11:00:55 -0800

    Encode

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,24 +8,26 @@
 
 char *leet(char *s)
 {
-	int i;
-	/*
-	 * char encrypt[5][3] = {{'o', 'O', '0'}, {'l', 'L', '1'}, {'e', 'E', '3'},
-	 *	{'a', 'A', '4'}, {'t', 'T', '7'}};
-	 */
+	int i, j, k;
+	
+	char encrypt[5][3] = {{'o', 'O', '0'}, {'l', 'L', '1'}, {'e', 'E', '3'},
+		{'a', 'A', '4'}, {'t', 'T', '7'}};
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		while (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		while (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		while (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
-		while (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		while (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
+		j = 0;
+		k = 0;
+
+		while(j < 5 && k < 3)
+		{
+			if(s[i] == encrypt[j][k])
+			{
+				s[i] = encrypt[j][2];
+				break;
+			}
+			k++;
+			j++;
+		}
 	}
 	return (s);
 }
