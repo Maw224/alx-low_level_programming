640783a620b6eaa084df85915ced70f164f089d7
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-03-01 08:36:06 -0800

    String concat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - main block
+ * @dest: string
+ * @src: string
+ * Return: pointer to strings concat
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
