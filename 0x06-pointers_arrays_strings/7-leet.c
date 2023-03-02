c035500034ece20896f57ea731bcbe7cdb1331ff
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-03-01 16:50:25 -0800

    Encode

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "search.h"
 
 /**
  * leet - main block
@@ -15,27 +16,4 @@ char *leet(char *s)
 		s[i] = search(s[i]);
 	}
 	return (s);
-}
-
-/**
- * search - secondary block
- * @c: character
- * Return: character
- */
-
-int search(char c)
-{
-	int i, j;
-	char encrypt[5][3] = {{'o', 'O', '0'}, {'l', 'L', '1'}, {'e', 'E', '3'},
-		{'a', 'A', '4'}, {'t', 'T', '7'}};
-
-	for (i = 0; i < 5; i++)
-	{
-		for (j = 0; j < 2; j++)
-		{
-			if (c == encrypt[i][j])
-				c = encrypt[i][2];
-		}
-	}
-	return (c);
 }
