45fb2e81855a08dc20f15b59a747221575ea5376
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-03-02 14:16:07 -0800

    Addition

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int checkDone(int, int, char *, int, int, int);
+
 /**
  * infinite_add - main block
  * @n1: first number
@@ -45,9 +47,34 @@ char *infinite_add(char *n1, char *n2, char *r, int size)
 				? ((n1[len1 - i - 1] - '0') + (n2[len2 - i - 1] - '0') + rem) / 10
 				: ((n1[len1 - i - 1] - '0') + (n2[len2 - i - 1] - '0')) / 10;
 		}
+		if (checkDone(len1, len2, r, size, i, rem)
+				break;
 	}
 	if (len3 >= size)
 		return (0);
 	printf("%d is length\n", len3);
 	return (r);
+}
+
+/**
+ * checkDone - secondary block
+ * @len1: num
+ * @len2: num
+ * @r: str
+ * @size: num
+ * @i: num
+ * @rem: num
+ *
+ * Return: 1 or 0
+ */
+
+int checkDone(int len1, int len2, char *r, int size, int i, int rem)
+{
+	if ((len1 - i - 1) < 0 && (len2 - i - 1) < 0)
+	{
+		*(r + (size - i - 1)) = rem;
+		return (1);
+	}
+	else
+		return (0);
 }
