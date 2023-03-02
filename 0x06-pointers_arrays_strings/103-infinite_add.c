a80a13277efc86bc9ec57ff42eac0775ed171a5e
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-03-01 19:33:44 -0800

    Addition

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -14,7 +14,7 @@
 char *infinite_add(char *n1, char *n2, char *r, int size)
 {
 	int len1 = strlen(n1), len2 = strlen(n2);
-	int i;
+	int i, rem;
 
 	if (size <= len1 || size <= len2)
 		return (0);
@@ -22,11 +22,23 @@ char *infinite_add(char *n1, char *n2, char *r, int size)
 	for (i = 0; i < size; i++)
 	{
 		if ((len1 - i - 1) < 0 && (len2 - i - 1) >= 0)
-			r[size - i - 1] = n2[len2 - i - 1];
+		{
+			r[size - i - 1] = rem != '0' ? (n2[len2 - i - 1] + rem) % 10
+				: n2[len2 - i - 1] % 10;
+			rem = rem != '0' ? (n2[len2 - i - 1] + rem) / 10 : '0';
+		}
 		else if ((len2 - i - 1) < 0 && (len1 - i - 1) >= 0)
-			r[size - i - 1] = n1[len1 - i - 1];
+		{
+			r[size - i - 1] = rem != '0' ? (n1[len1 - i - 1] + rem) % 10
+				: n1[len1 - i - 1] % 10;
+			rem = rem != '0' ? (n1[len1 - i - 1] + rem) / 10 : '0'; 
 		else if ((len1 - i - 1) >= 0 && (len2 - i - 1) >= 0)
-			r[size - i - 1] = n1[len1 - i - 1] + n2[len2 - i - 1];
+		{
+			r[size - i - 1] = rem != '0' ? (n1[len1 - i - 1] + n2[len2 - i - 1] + rem) % 10
+				: (n1[len1 - i - 1] + n2[len2- i - 1]) % 10;
+			rem = rem != '0' ? (n1[len1 - i - 1] + n2[len2 - i - 1] + rem) / 10
+				: (n1[len1 - i - 1] + n2[len2 - i - 1]) / 10;
+		}
 	}
 
 	return (r);
