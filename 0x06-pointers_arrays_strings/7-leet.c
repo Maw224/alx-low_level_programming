85eca44c529ea89f7bda534132122256dbebed30
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-03-01 16:02:12 -0800

    Encode

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -13,14 +13,18 @@ char *leet(char *s)
 	char encrypt[5][3] = {{'o', 'O', '0'}, {'l', 'L', '1'}, {'e', 'E', '3'},
 		{'a', 'A', '4'}, {'t', 'T', '7'}};
 
-	for (i = 0, j = 0; s[i] != '\0' || j < 5; i++, j++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (k = 0; k < 2; k++)
+		for (j = 0, k = 0; j < 5 && k < 2; k++)
 		{
 			if (s[i] == encrypt[j][k])
 			{
 				s[i] = encrypt[j][2];
-				j = 0;
+			}
+			else if (k == 2)
+			{
+				k = 0;
+				j++;
 			}
 		}
 	}
