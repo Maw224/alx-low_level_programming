e3f628970cbee1c6be024a6b4de7492324fe35fd
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-03-01 17:42:24 -0800

    print number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_number - main block
+ * @n: number
+ */
+
+void print_number(int n)
+{
+	int i = 0, hld = n, max = 1, count = 0;
+
+	while (i == 0)
+	{
+		if (hld / 10 == 0)
+			i = 1;
+		else
+		{
+			max *= 10;
+			hld /= 10;
+		}
+		count++;
+	}
+
+	if (n < 0)
+		_putchar('-');
+
+	hld = n;
+	for (i = 0; i < count; i++)
+	{
+		if (i == (count - 1))
+			_putchar('0' + (hld % 10));
+		else
+			_putchar('0' + (hld / max));
+
+		max /= 10;
+		hld -= (hld / max) * max;
+	}
+}
