a306151defaf43d212dfae19fd921feba2578a15
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-03-01 17:02:48 -0800

    Encode

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,11 +9,20 @@
 
 char *leet(char *s)
 {
-	int i;
+	int i = 0, j = 0;
+	char letters[5] = {'o', 'l', 'e', 'a', 't'};
+	char leet[5] = {'0', '1', '3', '4', '7'};
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[i])
 	{
-		s[i] = search(s[i]);
+		j = 0;
+		while (j < 5)
+		{
+			if (s[i] == letters[j] || s[i] == letters[j] - 32)
+				s[i] = leet[j];
+			j++;
+		}
+		i++;
 	}
 	return (s);
 }
