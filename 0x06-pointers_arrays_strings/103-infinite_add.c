e1eb3dc7d1955814e6694a7723fa8e1598cd5f19
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-03-01 18:43:11 -0800

    Infinite Add

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * infinte_add - main block
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer to save the result
+ * @size: buffer size
+ * Return: pointer to result
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size)
+{
+	int len1 = strlen(n1), len2 = strlen(n2);
+
+	if (size >= len1 || size >= len2)
+		return (0);
+
+	sprintf(r, "%lu", atoi(n1) + atoi(n2));
+	return (r);
+}
