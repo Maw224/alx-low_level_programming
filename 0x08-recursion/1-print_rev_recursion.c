593a49234bdaff0d546f7cf17eef2c7c13cd52f7
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-03-08 04:47:49 -0800

    print rev

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - main block
+ * @s: string
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s++);
+		_putchar(*s);
+	}
+}
