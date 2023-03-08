bf744db44ad0d712bb39d2fe728fd0d71d13d8b5
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-03-08 07:21:08 -0800

    Palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,31 @@
+#include "main.h"
+int _pal(char *, int, int);
+
+/**
+ * is_palindrome - main block
+ * @s: string
+ * Return: 1 pal, 0 otherwise
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	return (_pal(s, 0, strlen(s) - 1));
+}
+
+/**
+ * _pal - secondary block
+ * @s: string,
+ * @i: first index
+ * @j: last index
+ * Return: 1 or 0
+ */
+
+int _pal(char *s, int i, int j)
+{
+	if (s[i] != s[j])
+		return (0);
+	else
+		return (_pal(s, ++i, --j));
+}
