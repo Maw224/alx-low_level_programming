6c08f523e0988a529acc2407ab021f8204be536c
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-03-09 21:53:30 -0800

    Print program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * @argc: number of arguments
+ * @argv: pointer to arguments
+ * Return: 0 success
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
