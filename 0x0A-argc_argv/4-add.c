b51e3bfd3a22519d21185033f62a59715409d338
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-03-10 07:12:02 -0800

    Add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 /**
  * main - mainblock
@@ -18,16 +17,10 @@ int main(int argc, char **argv)
 	{
 		if ((*argv[i] < 48 || *argv[i] > 57) && !(argv[i][0] == '-'))
 		{
-			checked = false;
-			break;
+			printf("Error\n");
+			return (1);
 		}
 		total += atoi(argv[i]);
-	}
-
-	if (!checked)
-	{
-		printf("Error\n");
-		return (1);
 	}
 
 	printf("%d\n", total);
