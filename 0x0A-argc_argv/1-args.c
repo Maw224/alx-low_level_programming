f06b3ed380ab9afa26d229eea8c61e179e09e1e5
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-03-09 21:55:52 -0800

    Arguments number

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - mainblock
+ * @argc: number of arguments
+ * @argv: arg values
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
