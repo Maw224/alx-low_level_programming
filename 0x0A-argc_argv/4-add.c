00e450e0a876c1fd1eb03b587fa07bf429701b79
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-03-10 06:59:16 -0800

    Add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -22,7 +22,7 @@ int main(int argc, char **argv)
 
 	for (i = 1; i < argc; i++)
 	{
-		if (*argv[i] < 48 || *argv[i] > 57)
+		if ((*argv[i] < 48 || *argv[i] > 57) && !(argv[i][0] == '-'))
 		{
 			checked = false;
 			break;
