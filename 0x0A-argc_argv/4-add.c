6d20b3623c5e89ba651d687b5c89894dcce4002f
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-03-09 22:18:02 -0800

    Addition

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - mainblock
+ * @argc: num of args
+ * @argv: args vals
+ * Return: 1 fail, 0 success
+ */
+
+int main(int argc, char **argv)
+{
+	int total = 0, i;
+	bool checked = true;
+
+	if (!argc || argc < 1)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (isdigit(argv[i]))
+			total += argv[i];
+		else
+		{
+			checked = false;
+			break;
+		}
+	}
+
+	if (!checked)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", total);
+	return (0);
+}
