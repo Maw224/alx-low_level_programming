b864a727765dcfb6c44a57250563409f20a530ed
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-03-13 01:38:41 -0700

    create array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - main block
+ * @size: size of array
+ * @c: character to fill the array
+ * Return: pointer ti array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	int i;
+
+	if (size == 0)
+		return (NULL);
+	arr = malloc(size * sizeof(c));
+	if (arr = NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		arr[i] = c;
+	return (arr);
+}
