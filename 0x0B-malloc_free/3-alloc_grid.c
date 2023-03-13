43e2a7515eca46d11806e2553b7bbb68c792d970
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-03-13 02:32:06 -0700

    2d array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - main.block
+ * @w: width
+ * @h: height
+ * Return: pointer to 2D array
+ */
+
+int **alloc_grid(int w, int h)
+{
+	int *arr;
+
+	if (w <= 0 || h <= 0)
+		return (NULL);
+
+	arr = calloc(w * h, sizeof(int));
+
+	if (!arr)
+		return (NULL);
+	return (arr);
+
+}
