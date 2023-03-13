164413ba5b8d04161b5d8edee553db4a13413e7c
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-03-13 01:52:07 -0700

    Reallocation

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,10 +10,14 @@
 char *_strdup(char *str)
 {
 	char *dup;
+	int len = 0;
 
 	if (str == NULL)
 		return (NULL);
-	dup = realloc(str, sizeof(str));
+
+	while (*str++)
+		len ++;
+	dup = realloc(str, len);
 
 	if (!dup)
 		return (NULL);
