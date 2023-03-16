c626bc6b8689f0c4ef01649a05ae7ebebb71a92d
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-03-16 03:34:56 -0700

    Calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * _calloc - main block
@@ -12,7 +13,6 @@
 void *_calloc(unsigned int n, unsigned int s)
 {
 	void *arr;
-	int *ARR;
 	unsigned int i;
 
 	if (n <= 0 || s <= 0)
@@ -23,10 +23,7 @@ void *_calloc(unsigned int n, unsigned int s)
 	if (!arr)
 		return (NULL);
 
-	ARR = arr;
-
-	for (i = 0; i < (n * s); i++)
-		ARR[i] = 0;
+	memset(arr, 0, n * s);
 
 	return (arr);
 }
