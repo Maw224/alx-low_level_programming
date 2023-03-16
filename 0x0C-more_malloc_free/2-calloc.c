fb04e3871a2129a926ce250ab85677f17853a62f
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-03-16 03:13:12 -0700

    Calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - main block
+ * @n: members
+ * @s: size
+ *
+ * Return: pointer to location
+ */
+
+void *_calloc(unsigned int n, unsigned int s)
+{
+	void *arr;
+
+	if (n <= 0 || s <= 0)
+		return (NULL);
+
+	arr = malloc (n * s);
+
+	if (!arr)
+		return (NULL);
+
+	return (arr);
+}
