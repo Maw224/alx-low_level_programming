793d76d0db5ce8ca858b816e048a4f0424cf2b70
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-03-16 03:50:28 -0700

    Range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,17 +10,17 @@
 
 int *array_range(int min, int max)
 {
-	int *arr, i;
+	int *arr, i = 0;
 
 	if (min >= max)
 		return (NULL);
 
-	arr = malloc(max - min + 1);
+	arr = malloc((max - min + 1) * sizeof(int));
 	if (!arr)
 		return (NULL);
 
-	for (i = 0; min <= max; i++, min++)
-		arr[i] = min;
+	while (min <= max)
+		arr[i++] = min++;
 
 	return (arr);
 }
