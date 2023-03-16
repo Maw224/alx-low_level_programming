41bb929057a073a65a2a1a40df43a892c1616844
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-03-16 04:11:37 -0700

    Realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,6 +17,7 @@ void *_realloc(void *ptr, unsigned int old_s, unsigned int new_s)
 
 	if (new_s == old_s)
 		return (ptr);
+	
 	if (new_s == 0 && ptr)
 	{
 		free(ptr);
@@ -29,16 +30,19 @@ void *_realloc(void *ptr, unsigned int old_s, unsigned int new_s)
 
 		if (!new)
 			return (NULL);
+	
 		return (new);
 	}
 
 	new = malloc(new_s);
 
 	if (!new)
+	{
+		free(ptr);
 		return (NULL);
+	}
 
 	memcpy(new, ptr, new_s);
 	free(ptr);
-
 	return (new);
 }
