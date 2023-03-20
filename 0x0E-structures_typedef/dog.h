10b008fe13f838e36146c5db469798f9b228893a
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-03-20 02:24:05 -0700

    Dog header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 
-/*
- * File: dog.h
- * Auth: Moawia Ahmed
- * Desc: header file for dog structure
- */
-
 /**
  * struct dog - new dog struct type
  * @name: dog name
@@ -18,6 +12,17 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-}
+};
+
+/**
+ * dog_t - typedef struct dog type
+ */
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog, char *, float, char *);
+void print_dog(struct dog);
+dog_t *new_dog(char *, float, char *);
+void free_dog(dog_t *);
 
 #endif
