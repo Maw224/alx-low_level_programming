e17cc6b174f252548bb3afc21f42f8624bf31e2f
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-03-20 02:35:44 -0700

    Dog init

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -25,7 +25,7 @@ struct dog
  */
 typedef struct dog dog_t;
 
-void init_dog(struct dog, char *, float, char *);
+void init_dog(struct dog *, char *, float, char *);
 void print_dog(struct dog);
 dog_t *new_dog(char *, float, char *);
 void free_dog(dog_t *);
