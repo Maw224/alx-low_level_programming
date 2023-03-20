4f2a4d542899638b8be5c605cf2f7210caf7a202
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-03-20 02:58:34 -0700

    New Dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,4 +1,6 @@
 #include "dog.h"
+#include "stdlib.h"
+#include "string.h"
 
 /**
  * new_dog - main block
@@ -12,8 +14,23 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
 
+	new_dog = malloc(sizeof(dog_t));
 	if (!new_dog)
 		return (NULL);
+
+	new_dog->name = malloc(strlen(name) * sizeof(char));
+	if (!new_dog->name)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+
+	new_dog->owner = malloc(strlen(owner) * sizeof(char));
+	if (!new_dog->owner)
+	{
+		free(new_dog);
+		return (NULL);
+	}
 
 	new_dog->name = name;
 	new_dog->age = age;
