a03f8000b639136bee92e10d277364637fa5ef46
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-03-20 02:17:42 -0700

    Dog header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -21,15 +21,4 @@ struct dog
 	char *owner;
 }
 
-/**
- * dog_t - typedef struct dog type
- */
-
-typedef struct dog dog_t;
-
-void init_dog(struct dog, char *, float, char *);
-void print_dog(struct dog);
-dog_t *new_dog(char *, float, char *);
-void free_dog(dog_t *);
-
 #endif
