63602c5e7a1e724375ec7382ebfebe4fbcea2cbb
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-03-20 03:02:04 -0700

    New dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -32,9 +32,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	new_dog->name = name;
+	new_dog->name = strcpy(new_dog->name, name);
 	new_dog->age = age;
-	new_dog->owner = owner;
+	new_dog->owner = strcpy(newdog->owner, owner);
 
 	return (new_dog);
 }
