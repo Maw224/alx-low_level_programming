869842dd309d5ee340f6f8bad64527cfe528e246
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-03-20 02:41:22 -0700

    Print dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -26,7 +26,7 @@ struct dog
 typedef struct dog dog_t;
 
 void init_dog(struct dog *, char *, float, char *);
-void print_dog(struct dog);
+void print_dog(struct dog *);
 dog_t *new_dog(char *, float, char *);
 void free_dog(dog_t *);
 
