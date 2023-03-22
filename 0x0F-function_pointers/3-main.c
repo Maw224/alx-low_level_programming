80dcd9c7899b35904b7f8a3ae5386171b1f1112d
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-03-22 03:29:24 -0700

    operator functions

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - main block
+ * @argc: num of args
+ * @argv: args
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int num1, num2;
+	char *op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op = argv[2];
+
+	if (get_op_func(op) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*op == '/' || *op == '%') && num2 == '0')
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(op)(num1, num2));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - add 2 nums
+ * @a: number
+ * @b: number
+ * Return: a + b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - sub 2 nums
+ * @a: number
+ * @b: number
+ * Return: a - b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - mul 2 nums
+ * @a: number
+ * @b: number
+ * Return: a * b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - div 2 nums
+ * @a: number
+ * @b: number
+ * Return: a / b
+ */
+
+int op_div(int a, int b)
+{
+        return (a / b);
+}
+
+/**
+ * op_mod - mod 2 nums
+ * @a: number
+ * @b: number
+ * Return: a % b
+ */
+
+int op_mod(int a, int b)
+{
+        return (a % b);
+}
