b8149a9b33fb121f4354a7481c907d60001208bf
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-03-22 03:45:40 -0700

    Operators fucntions

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -29,7 +29,7 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(99);
 	}
-	if ((*op == '/' || *op == '%') && num2 == '0')
+	if ((*op == '/' || *op == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
