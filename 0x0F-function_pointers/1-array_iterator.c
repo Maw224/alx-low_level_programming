48daf3450d3f06e3fc8f0ff12feaade0c41bd341
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-03-22 01:50:13 -0700

    Compare

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,15 +2,15 @@
 
 /**
  * array_iterator - main block
- * arr: array of ints
- * s: size of arr
- * action: pointer to function
+ * @arr: array of ints
+ * @s: size of arr
+ * @action: pointer to function
  */
 
 void array_iterator(int *arr, size_t s, void (*action)(int))
 {
 	unsigned int i;
 
-	for(i = 0; i < s; i++)
+	for (i = 0; i < s; i++)
 		action(arr[i]);
 }
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - main block
+ * @arr: array of int
+ * @s: size of arr
+ * @cmp: pointer to function
+ * Return: index of compare match
+ */
+
+int int_index(int *arr, int s, int (*cmp)(int))
+{
+	int i;
+
+	if (s <= 0)
+		return (-1);
+
+	for (i = 0; i < s; i++)
+	{
+		if (cmp(arr[i]))
+			return (i);
+	}
+
+	return (-1);
+}
