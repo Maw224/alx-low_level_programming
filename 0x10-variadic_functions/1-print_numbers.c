3dcce2ddcfbf83a4094fd00109cf4ad39a480317
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-03-24 03:12:21 -0700

    Print numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * print_numbers - main block
+ * @n: number of args
+ * @s: seperator
+ * @...: args
+ */
+
+void print_numbers(const char *s, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		if (s)
+			printf("%d%s", va_arg(args, int), s);
+		else
+			printf("%d", va_arg(args, int));
+	}
+	va_end(args);
+}
