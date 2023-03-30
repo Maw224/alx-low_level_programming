8db3f30eca6ff93ba494c53b3b730487556f0c3c
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-03-30 13:12:31 -0700

    Assembly printf

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,13 @@
-mov rdi, fmt
+SECTION .data
+str_fmt: db "%s", 10, 0
+message: db "Hello, Holberton", 0
+
+SECTION .text
+extern printf
+global main
+
+main:
+mov rdi, str_fmt
 mov rsi, message
 mov al, 0
-
-extern printf
 call printf
-
-fmt: db "%s", 0
-message: db "Hello, Holberton", 0
