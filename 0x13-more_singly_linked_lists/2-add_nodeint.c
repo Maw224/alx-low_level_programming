0397fc03664d228181fa7a646744e50ac9a4b3ac
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-04-03 06:02:51 -0700

    Add node

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - main block
+ * @h: list head
+ * @n: number to be added to the node
+ * Return: pointer to the new node
+ */
+
+listint_t *add_nodeint(listint_t **h, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+
+	if(!new)
+		return (NULL);
+
+	new->n = n;
+	new->next = *h;
+	*h = new;
+	return (new);
+}
