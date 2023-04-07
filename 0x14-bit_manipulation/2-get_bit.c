c7389d11db461239b68f4e216e0d8823c42c8481
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-04-07 09:37:46 -0700

    Get bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * get_bit - main block
+ * @n: number
+ * @index: index of the bit to be returned
+ * Return: bit at index
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int ones = 1, idx = 0;
+
+	while (n || ones)
+	{
+		if (idx == index)
+			return ((n & 1));
+		n >>= 1;
+		idx++;
+		ones = 0;
+	}
+	return (-1);
+}
