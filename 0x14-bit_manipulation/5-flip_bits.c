afed43cd0f02cf73c1ea14445b16765a077315ef
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-04-07 10:24:44 -0700

    Flip bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * flip_bits - main block
+ * @n: first number
+ * @m: second number
+ * Return: num of bits
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int bits = 0, res = n ^ m;
+
+	while (res)
+	{
+		bits += res & 1;
+		res >>= 1;
+	}
+
+	return (bits);
+}
