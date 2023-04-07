c1e32133c6a80a6c6630805d9de9cf476dd8d98e
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-04-07 09:04:57 -0700

    Binary to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * binary_to_uint - main block
+ * @b: binary stream
+ * Return: decimal representation
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int dec = 0, twos = 1;
+	int len = 0;
+
+	if (!b)
+		return (0);
+
+	while (b[len])
+		len++;
+
+	for (len -= 1; len >= 0; len--)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+
+		dec += (b[len] - '0') * twos;
+		twos *= 2;
+	}
+	return (dec);
+}
