8c202f36f5fea7af5de9f0d56c45ef1151c5269a
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-04-08 09:47:56 -0700

    Create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * create_file - main block
+ * @filename: file name
+ * @text_content: text content
+ * Return: 1 success, -1 failur
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t o, w;
+	size_t letters = 0;
+
+	if (!filename)
+		return (-1);
+
+	while (text_content[letters])
+		letters++;
+
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, letters);
+
+	if (o == -1 || w == -1)
+		return (-1);
+	close(o);
+	return (1);
+}
