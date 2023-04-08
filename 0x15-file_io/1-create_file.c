e63b78a118b0899e24fb72115476315ce525c024
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-04-08 09:50:04 -0700

    Create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,8 +15,11 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	while (text_content[letters])
-		letters++;
+	if (!text_content)
+	{
+		while (text_content[lettes])
+			letters++;
+	}
 
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(o, text_content, letters);
