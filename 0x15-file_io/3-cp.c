6707ce08c7819af0f13dc8f6018f1c7e4ee416c9
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-04-08 16:08:36 -0700

    Copy from file to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,25 +1,30 @@
 #include "main.h"
 #include <stdio.h>
 
+int close_file(int);
+char *buf_create(char *);
+
 /**
  * main - main block
  * @argc: num of args passed
  * @argv: args
- * Return: 0 success, 97 wrong num of args, 98 cant read file, 99 cant write to file, 100 cant close
+ * Return: 0 success, 97 wrong num of args,
+ *	98 cant read file, 99 cant write to file, 100 cant close
  */
 
 int main(int argc, char **argv)
 {
-	ssize_t o1, o2, r, w, c1, c2;
+	ssize_t o1, o2, r, w;
 	char *buf;
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO,
+				"Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
-	buf = malloc(sizeof(char) * 1024);
+	buf = buf_create(argv[2]);
 
 	o1 = open(argv[1], O_RDONLY);
 	o2 = open(argv[2], O_CREAT | O_WRONLY, 0664);
@@ -29,7 +34,8 @@ int main(int argc, char **argv)
 	{
 		if (o1 == -1 || r == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			dprintf(STDERR_FILENO,
+					"Error: Can't read from file %s\n", argv[1]);
 			free(buf);
 			exit(98);
 		}
@@ -37,7 +43,8 @@ int main(int argc, char **argv)
 		w = write(o2, buf, r);
 		if (w == -1 || o2 == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO,
+					"Error: Can't write to %s\n", argv[2]);
 			free(buf);
 			exit(99);
 		}
@@ -46,13 +53,48 @@ int main(int argc, char **argv)
 		o2 = open(argv[2], O_WRONLY | O_APPEND);
 	}
 	free(buf);
-	c1 = close(o1);
-	c2 = close(o2);
-	if (c1 == -1 || c2 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", (c1 || c2));
-		exit(100);
-	}
+	close_file(o1);
+	close_file(o2);
 
 	return (0);
+}
+
+/**
+ * close_file - close an opened file
+ * @f: file
+ * Return: result
+ */
+
+int close_file(int f)
+{
+	int c = close(f);
+
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO,
+				"Error: Can't close fd %d\n", c);
+		exit(100);
+	}
+	return (c);
+}
+
+/**
+ * buf_create - create buffer
+ * @f: file to be written in
+ * Return: pointer to the buffer
+ */
+
+char *buf_create(char *f)
+{
+	char *buf;
+
+	buf = malloc(sizeof(char) * 1024);
+	if (!buf)
+	{
+		dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", f);
+		exit(99);
+	}
+
+	return (buf);
 }
